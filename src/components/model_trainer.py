aecfa5b27df9f71989791bb6b642a0dd5ff1fa49
Viral Sherathiya <101617198+Viral3899@users.noreply.github.com>
2023-03-17 12:13:15 +0530

    Update model_trainer.py

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -91,7 +91,7 @@ class ModelTrainer:
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
             ]
-            best_model = models[best_model_name]
+            best_model = models[best_model_name]['model']
 
             if best_model_score<0.6:
                 raise CustomeException("No best model found")
@@ -108,43 +108,7 @@ class ModelTrainer:
             return r2_square
 
 
-            # models = {
-            #     "Random Forest": RandomForestRegressor(),
-            #     "Decision Tree": DecisionTreeRegressor(),
-            #     "Gradient Boosting": GradientBoostingRegressor(),
-            #     "Linear Regression": LinearRegression(),
-            #     "K-Neighbors Classifier": KNeighborsRegressor(),
-            #     "XGBClassifier": XGBRegressor(),
-            #     "CatBoosting Classifier": CatBoostRegressor(verbose=False),
-            #     "AdaBoost Classifier": AdaBoostRegressor(),
-            # }
-
-            # model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
-            #                                  models=models)
-            
-            # ## To get best model score from dict
-            # best_model_score = max(sorted(model_report.values()))
-
-            # ## To get best model name from dict
-
-            # best_model_name = list(model_report.keys())[
-            #     list(model_report.values()).index(best_model_score)
-            # ]
-            # best_model = models[best_model_name]
-
-            # if best_model_score<0.6:
-            #     raise CustomeException("No best model found")
-            # logging.info(f"Best found model on both training and testing dataset")
-
-            # save_object(
-            #     file_path=self.model_trainer_config.train_model_file_path,
-            #     obj=best_model
-            # )
-
-            # predicted=best_model.predict(X_test)
-
-            # r2_square = r2_score(y_test, predicted)
-            # return r2_square
+          
         
         except Exception as e:
             logging.info(f'Error Occured {CustomeException(e,sys)}')
