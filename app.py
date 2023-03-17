b742bbe40ffc894dcadc57b76ee69af1b4bcf0a9
Viral Sherathiya <101617198+Viral3899@users.noreply.github.com>
2023-03-17 16:55:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,27 +24,22 @@ def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
     else:
-        try:
-            data=CustomeData(
-                gender=request.form.get('gender'),
-                race_ethnicity=request.form.get('ethnicity'),
-                parental_level_of_education=request.form.get('parental_level_of_education'),
-                lunch=request.form.get('lunch'),
-                test_preparation_course=request.form.get('test_preparation_course'),
-                reading_score=float(request.form.get('writing_score')),
-                writing_score=float(request.form.get('reading_score'))
-            )
+        data=CustomeData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=float(request.form.get('writing_score')),
+            writing_score=float(request.form.get('reading_score'))
+        )
 
-            pred_input_df=data.get_data_as_data_frame()
-            logging.info(f"Data for Prediction is {pred_input_df.values}")
-            predict_pipeline=PredictPipeline()
-            results=predict_datapoint.predict(pred_input_df)
-            return render_template('home.html',results=results[0])
+        pred_input_df=data.get_data_as_data_frame()
+        logging.info(f"Data for Prediction is {pred_input_df.values}")
+        predict_pipeline=PredictPipeline()
+        results=predict_datapoint.predict(pred_input_df)
+        return render_template('home.html',results=results[0])
 
-
-
-        except Exception as e:
-            logging.info(f'Error Occured {CustomeException(e,sys)}')
 
 
 if __name__=="__main__":
